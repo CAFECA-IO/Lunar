b0cd836efbb0a951aaa59cc9c46760456e56252f
Luphia Chang <luphia@cafeca.io>
2021-10-26 15:08:41 +0800

    Support ERC20 token full name

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/lunar.js',
+  entry: ['regenerator-runtime/runtime.js',  './src/lunar.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'lunar.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+        ]
+      }
+    ]
+  }
 };
